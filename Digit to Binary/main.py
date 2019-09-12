bc6682b3573258d504ba4626bec9775fdb19a26e
Evan <evanojackson@gmail.com>
2019-09-12 08:37:33 -0500

    list reference

--- Digit to Binary/main.py
+++ Digit to Binary/main.py
@@ -7,13 +7,12 @@ def makeBinary(digit, binary):
         else:
             binary.append(1)
             makeBinary(0, binary)
-    return binary
 
 def main():
     digit = int(input("Enter in a Base 10 Number to see Binary, or -1 to exit: "))
     while digit > 0:
         binary = []
-        binary = makeBinary(digit, binary)
+        makeBinary(digit, binary)
         binary.reverse()
         print(str(digit) + " = ",end='')
         print(*binary)
