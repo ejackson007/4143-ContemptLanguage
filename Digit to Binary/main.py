d50ac7a61f078ee24254e33c582e665dac52e632
Evan <evanojackson@gmail.com>
2019-09-12 08:44:05 -0500

    comments added

--- Digit to Binary/main.py
+++ Digit to Binary/main.py
@@ -2,7 +2,7 @@ def makeBinary(digit, binary):
     if digit is not 0:
         if digit is not 1:
             binary.append(digit % 2)
-            print('.')
+            #integer division used to keep from getting decimals
             makeBinary(digit // 2, binary)
         else:
             binary.append(1)
@@ -13,10 +13,12 @@ def main():
     while digit > 0:
         binary = []
         makeBinary(digit, binary)
-        binary.reverse()
-        print(str(digit) + " = ",end='')
-        print(*binary)
-        binary.clear()
+        binary.reverse()    #must reverse list before print for correct order
+        print(str(digit) + " = ",end='') #print without next line
+        #Did not let me use *list as a variable in conjunction with another variable
+        #so i gave it its own print function
+        print(*binary)  
+        binary.clear()  #clear list for future use
         digit = int(input("Enter in a Base 10 Number to see Binary, or -1 to exit: "))
 
 if __name__ == '__main__':
