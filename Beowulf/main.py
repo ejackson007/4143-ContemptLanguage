b1b7981ea734f55c7da918b24f517d7e0fe39905
Evan <evanojackson@gmail.com>
2019-10-09 01:44:49 -0500

    change to aux

--- Beowulf/main.py
+++ Beowulf/main.py
@@ -9,23 +9,26 @@ def main():
     'float' : 'ship',
     'carle' : 'hero'}
 
+    #list to keep count of how many changes are made
     count = [0,0,0,0]
 
-    i = 0
+    #opens the file and cuts it into the needed section, and put into auxillary text file
     with open("/Users/evan/4143-ContemptLanguage/Beowulf/beowulf.txt", 'r+') as f:
-        with open("/Users/evan/4143-ContemptLanguage/Beowulf/output.txt", 'r+') as o:
-            for _ in range(933):
+        with open("/Users/evan/4143-ContemptLanguage/Beowulf/auxillary.txt", 'r+') as o:
+            for _ in range(933): #starts at "BEOWULF."
                 next(f)
             for line in f:
-                if line is "ADDENDA.":
+                if line.find("ADDENDA.") is not -1: #goes until "ADDENDA."
+                    o.write(line)
                     break
                 else:
                     o.write(line)
 
-    with open("/Users/evan/4143-ContemptLanguage/Beowulf/output.txt", 'r+') as f:
+    #opens auxillary for editing, and put into beowulf2
+    with open("/Users/evan/4143-ContemptLanguage/Beowulf/auxillary.txt", 'r+') as f:
         with open("/Users/evan/4143-ContemptLanguage/Beowulf/beowulf2.txt", 'r+') as o:   
             for line in f:
-                for key in translation.keys():
+                for key in translation.keys(): #check each line for translation dictionary
                     if line.find(key) is not -1:
                         line = line.replace(key, translation[key])
                         if key is 'bairn':
